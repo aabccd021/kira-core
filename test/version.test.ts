e230b052beee959aa24681be00dc9c632b4deb92
aabccd021 <aabccd021@gmail.com>
2021-05-15 19:33:03 +0700

    type use no union

--- test/version.test.ts
+++ test/version.test.ts
@@ -5,7 +5,6 @@ import { VERSION } from '../src/schema';
 describe('schema type version', () => {
   it('has same version as package', () => {
     const packageVersion = JSON.parse(readFileSync('package.json', 'utf-8'))?.version;
-    expect(VERSION).toHaveLength(1);
-    expect(VERSION[0]).toEqual(packageVersion);
+    expect(VERSION).toEqual(packageVersion);
   });
 });
