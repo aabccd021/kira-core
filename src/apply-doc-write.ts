2ac7fd2cb62f9b01a08b0c611c16345335670a87
aabccd021 <aabccd021@gmail.com>
2021-07-31 18:23:32 +0700

    apply-doc-write.test

--- src/apply-doc-write.ts
+++ src/apply-doc-write.ts
@@ -1,29 +1,15 @@
 import { Either, Failed, Failure, foldRight, ShouldBeUnreachableFailure, Value } from 'trimop';
 
 import { DateField, Doc, Field, NumberField, RefField, WriteDoc, WriteField } from './data';
-
-/**
- * ApplyWriteFailure
- */
-export type ApplyWriteFailure = {
-  readonly _failureType: 'ApplyWrite';
-  readonly expectedFieldTypes: readonly (Field['_type'] | WriteField['_type'] | 'undefined')[];
-  readonly field: Field | undefined;
-};
-
-export const ApplyWriteFailure: (p: Omit<ApplyWriteFailure, '_failureType'>) => ApplyWriteFailure =
-  (p) => ({
-    ...p,
-    _failureType: 'ApplyWrite',
-  });
+import { ApplyWriteDocFailure } from './failure/apply-write-doc-failure';
 
 export function applyFieldWrite({
-  oldField,
+  field,
   writeField,
 }: {
-  readonly oldField: Field | undefined;
+  readonly field: Field | undefined;
   readonly writeField: WriteField;
-}): Either<Failure, Field | undefined> {
+}): Either<Failure, Field> {
   if (writeField._type === 'string') {
     return Value(writeField);
   }
@@ -40,55 +26,68 @@ export function applyFieldWrite({
     return Value(DateField(new Date()));
   }
   if (writeField._type === 'increment') {
-    if (oldField === undefined || oldField._type === 'number') {
-      return Value(NumberField(oldField !== undefined ? oldField.value : 0 + writeField.value));
+    if (field === undefined || field._type === 'number') {
+      return Value(NumberField((field !== undefined ? field.value : 0) + writeField.value));
     }
     return Failed(
-      ApplyWriteFailure({
+      ApplyWriteDocFailure({
         expectedFieldTypes: ['number', 'undefined'],
-        field: oldField,
+        field,
       })
     );
   }
   if (writeField._type === 'ref') {
-    if (oldField?._type !== 'ref') {
-      return Failed(
-        ApplyWriteFailure({
-          expectedFieldTypes: ['ref'],
-          field: oldField,
-        })
+    if (field === undefined) {
+      return foldRight(
+        // eslint-disable-next-line no-use-before-define
+        applyDocWrite({ doc: {}, writeDoc: writeField.value.data }),
+        (newDoc) =>
+          Value(
+            RefField({
+              data: newDoc,
+              id: writeField.value.id,
+            })
+          )
+      );
+    }
+    if (field._type === 'ref') {
+      return foldRight(
+        // eslint-disable-next-line no-use-before-define
+        applyDocWrite({ doc: field.value.data, writeDoc: writeField.value.data }),
+        (newDoc) =>
+          Value(
+            RefField({
+              data: newDoc,
+              id: writeField.value.id,
+            })
+          )
       );
     }
 
-    return foldRight(
-      // eslint-disable-next-line no-use-before-define
-      applyDocWrite({ oldDoc: oldField.value.data, writeDoc: writeField.value.data }),
-      (newDoc) =>
-        Value(
-          RefField({
-            data: newDoc,
-            id: oldField.value.id,
-          })
-        )
+    return Failed(
+      ApplyWriteDocFailure({
+        expectedFieldTypes: ['ref'],
+        field,
+      })
     );
   }
   return Failed(ShouldBeUnreachableFailure(writeField));
 }
 
 export function applyDocWrite({
-  oldDoc,
+  doc,
   writeDoc,
 }: {
-  readonly oldDoc: Doc | undefined;
+  readonly doc: Doc | undefined;
   readonly writeDoc: WriteDoc;
 }): Either<Failure, Doc> {
   return Object.entries(writeDoc).reduce<Either<Failure, Doc>>(
     (acc, [fieldName, writeField]) =>
       foldRight(acc, (acc) =>
-        foldRight(applyFieldWrite({ oldField: oldDoc?.[fieldName], writeField }), (field) =>
-          Value(field === undefined ? acc : { ...acc, [fieldName]: field })
+        foldRight(applyFieldWrite({ field: doc?.[fieldName], writeField }), (field) =>
+          Value({ ...acc, [fieldName]: field })
         )
       ),
-    Value(oldDoc ?? {})
+    Value(doc ?? {})
   );
 }
---
+++ test/apply-doc-write.test.ts
@@ -0,0 +1,162 @@
+import 'jest-extended';
+
+import { Failed, ShouldBeUnreachableFailure, Value } from 'trimop';
+
+import {
+  CreationTimeField,
+  DateField,
+  Doc,
+  ImageField,
+  IncrementField,
+  NumberField,
+  RefField,
+  RefWriteField,
+  StringField,
+  WriteDoc,
+  WriteField,
+} from '../src';
+import { applyDocWrite } from '../src/apply-doc-write';
+import { ApplyWriteDocFailure } from '../src/failure/apply-write-doc-failure';
+import { almostEqualTimeBefore } from './util';
+
+describe('applyDocWrite', () => {
+  it('correctly apply write', () => {
+    const doc: Doc = {
+      age: NumberField(18),
+      group: RefField({
+        data: {
+          name: StringField('Keyakizaka46'),
+        },
+        id: '46',
+      }),
+      luckyNumber: NumberField(7),
+      name: StringField('Kira Masumoto'),
+      nickname: StringField('dorokatsu'),
+      profilePicture: ImageField({
+        url: 'https://sakurazaka46.com/images/14/eb2/a748ca8dac608af8edde85b62a5a8/1000_1000_102400.jpg',
+      }),
+    };
+    const writeDoc: WriteDoc = {
+      accountCreationTime: CreationTimeField(),
+      age: IncrementField(1),
+      birthday: DateField(new Date('2002-01-12T00:00:00Z')),
+      group: RefWriteField({
+        data: {
+          age: IncrementField(1),
+          logoPicture: ImageField({
+            url: 'https://sakurazaka46.com/files/14/s46/img/com-logo_sp.svg',
+          }),
+          name: StringField('Sakurazaka46'),
+        },
+        id: '46',
+      }),
+      hobby: StringField('Rubiks Cube'),
+      joinYear: NumberField(2020),
+      mentor: RefWriteField({
+        data: {
+          name: StringField('AkaneMoriya'),
+        },
+        id: '23',
+      }),
+      nickname: StringField('Kirako'),
+    };
+    expect(applyDocWrite({ doc, writeDoc })).toStrictEqual(
+      Value({
+        accountCreationTime: {
+          _type: 'date',
+          value: expect.toSatisfy(almostEqualTimeBefore(new Date())),
+        },
+        age: NumberField(19),
+        birthday: DateField(new Date('2002-01-12T00:00:00Z')),
+        group: RefWriteField({
+          data: {
+            age: NumberField(1),
+            logoPicture: ImageField({
+              url: 'https://sakurazaka46.com/files/14/s46/img/com-logo_sp.svg',
+            }),
+            name: StringField('Sakurazaka46'),
+          },
+          id: '46',
+        }),
+        hobby: StringField('Rubiks Cube'),
+        joinYear: NumberField(2020),
+        luckyNumber: NumberField(7),
+        mentor: RefWriteField({
+          data: {
+            name: StringField('AkaneMoriya'),
+          },
+          id: '23',
+        }),
+        name: StringField('Kira Masumoto'),
+        nickname: StringField('Kirako'),
+        profilePicture: ImageField({
+          url: 'https://sakurazaka46.com/images/14/eb2/a748ca8dac608af8edde85b62a5a8/1000_1000_102400.jpg',
+        }),
+      })
+    );
+  });
+  describe('IncrementField', () => {
+    it('fails to increment if previous value is not a NumberField', () => {
+      const doc: Doc = {
+        groupName: StringField('Sakurazaka46'),
+      };
+      const writeDoc: WriteDoc = {
+        groupName: IncrementField(2),
+      };
+      expect(applyDocWrite({ doc, writeDoc })).toStrictEqual(
+        Failed(
+          ApplyWriteDocFailure({
+            expectedFieldTypes: ['number', 'undefined'],
+            field: StringField('Sakurazaka46'),
+          })
+        )
+      );
+    });
+  });
+
+  describe('RefWriteField', () => {
+    it('fails to update if previous value is not a RefField', () => {
+      const doc: Doc = {
+        group: StringField('Keyakizaka46'),
+      };
+      const writeDoc: WriteDoc = {
+        group: RefWriteField({
+          data: {
+            name: StringField('Sakurazaka46'),
+          },
+          id: '46',
+        }),
+      };
+      expect(applyDocWrite({ doc, writeDoc })).toStrictEqual(
+        Failed(
+          ApplyWriteDocFailure({
+            expectedFieldTypes: ['ref'],
+            field: StringField('Keyakizaka46'),
+          })
+        )
+      );
+    });
+  });
+
+  it('should return shouldBeUnreachableFailure if given invalid field', () => {
+    const doc: Doc = {
+      group: StringField('Keyakizaka46'),
+    };
+    const writeField = { _type: 'impossibleWriteField' } as unknown as WriteField;
+    const writeDoc = { field: writeField };
+    expect(applyDocWrite({ doc, writeDoc })).toStrictEqual(
+      Failed(ShouldBeUnreachableFailure(writeField as never))
+    );
+  });
+
+  it('just write if previous doc is empty', () => {
+    const writeDoc: WriteDoc = {
+      name: StringField('Kira Masumoto'),
+    };
+    expect(applyDocWrite({ doc: undefined, writeDoc })).toStrictEqual(
+      Value({
+        name: StringField('Kira Masumoto'),
+      })
+    );
+  });
+});
